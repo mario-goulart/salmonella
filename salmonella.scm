3a62381268fb4f34f8887dcf57a6092fbfb64372
Mario Domenech Goulart <mario.goulart@gmail.com>
2012-02-22 19:30:33 -0200

    Fixed ugly hack to remove "-test" from chicken-install's args
    `-test' was being removed from anywhere in the args (even pathnames.
    It still is, but now the hack removes " -test" instead of "-test"
    (pathnames with spaces are not supported).

--- salmonella.scm
+++ salmonella.scm
@@ -214,7 +214,7 @@
                          chicken-install
                          (let ((args (chicken-install-args tmp-repo-dir)))
                            (or (irregex-replace ;; ugly hack to remove -test
-                                "-test" args "")
+                                " -test" args "")
                                args)))))))
         (if (and this-egg? (eq? action 'install)) ;; install this egg from this dir
             (install)
