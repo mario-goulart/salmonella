62753cd8937419907a328ef99e7c82731b2853fe
Mario Domenech Goulart <mario.goulart@gmail.com>
2013-01-17 19:46:47 -0200

    -h|--help command line options for salmonella-epidemy
    Reused from salmonella.

--- salmonella-epidemy.scm
+++ salmonella-epidemy.scm
@@ -69,88 +69,91 @@
     (system cmd)))
 
 
-(let* ((args (command-line-arguments))
-       (chicken-installation-prefix
-        (cmd-line-arg '--chicken-installation-prefix args))
-       (salmonella-prefix
-        (cmd-line-arg '--salmonella-prefix args))
-       (chicken-install-args
-        (cmd-line-arg '--chicken-install-args args))
-       (eggs-source-dir
-        (cmd-line-arg '--eggs-source-dir args))
-       (eggs-doc-dir
-        (cmd-line-arg '--eggs-doc-dir args))
-       (log-file (or (cmd-line-arg '--log-file args) "salmonella-epidemy.log"))
-       (skip-eggs (let ((skip (cmd-line-arg '--skip-eggs args)))
-                    (if skip
-                        (map string->symbol (string-split skip ","))
-                        '())))
-       (keep-repo? (and (member "--keep-repo" args) #t))
-       (repo-dir (and-let* ((path (cmd-line-arg '--repo-dir args)))
-                   (if (absolute-pathname? path)
-                       path
-                       (normalize-pathname
-                        (make-pathname (current-directory) path)))))
-       (log-dir (or (and-let* ((path (cmd-line-arg '--log-dir args)))
-                      (if (absolute-pathname? path)
-                          path
-                          (normalize-pathname
-                           (make-pathname (current-directory) path))))
-                    (mktempdir)))
-       (tmp-dir (or repo-dir (mktempdir)))
-       (eggs (remove (lambda (egg)
-                       (memq egg skip-eggs))
-                     (map string->symbol
-                          (remove (lambda (arg)
-                                    (string-prefix? "--" arg))
-                                  args))))
-       (total-eggs (length eggs))
-       (instances (or (and-let* ((i (cmd-line-arg '--instances args)))
-                        (or (string->number i) 1))
-                      1)))
+(let ((args (command-line-arguments)))
+  (when (or (member "-h" args)
+            (member "--help" args))
+    (usage exit-code: 0 epidemy?: #t))
+  (let* ((chicken-installation-prefix
+          (cmd-line-arg '--chicken-installation-prefix args))
+         (salmonella-prefix
+          (cmd-line-arg '--salmonella-prefix args))
+         (chicken-install-args
+          (cmd-line-arg '--chicken-install-args args))
+         (eggs-source-dir
+          (cmd-line-arg '--eggs-source-dir args))
+         (eggs-doc-dir
+          (cmd-line-arg '--eggs-doc-dir args))
+         (log-file (or (cmd-line-arg '--log-file args) "salmonella-epidemy.log"))
+         (skip-eggs (let ((skip (cmd-line-arg '--skip-eggs args)))
+                      (if skip
+                          (map string->symbol (string-split skip ","))
+                          '())))
+         (keep-repo? (and (member "--keep-repo" args) #t))
+         (repo-dir (and-let* ((path (cmd-line-arg '--repo-dir args)))
+                     (if (absolute-pathname? path)
+                         path
+                         (normalize-pathname
+                          (make-pathname (current-directory) path)))))
+         (log-dir (or (and-let* ((path (cmd-line-arg '--log-dir args)))
+                        (if (absolute-pathname? path)
+                            path
+                            (normalize-pathname
+                             (make-pathname (current-directory) path))))
+                      (mktempdir)))
+         (tmp-dir (or repo-dir (mktempdir)))
+         (eggs (remove (lambda (egg)
+                         (memq egg skip-eggs))
+                       (map string->symbol
+                            (remove (lambda (arg)
+                                      (string-prefix? "--" arg))
+                                    args))))
+         (total-eggs (length eggs))
+         (instances (or (and-let* ((i (cmd-line-arg '--instances args)))
+                          (or (string->number i) 1))
+                        1)))
 
-  (when (null? eggs)
-    (print "Nothing to do.")
-    (exit 0))
+    (when (null? eggs)
+      (print "Nothing to do.")
+      (exit 0))
 
-  ;; Remove the temporary directory if interrupted
-  (set-signal-handler! signal/int
-                       (lambda (signal)
-                         (delete-path tmp-dir)
-                         (delete-path log-dir)
-                         (exit)))
+    ;; Remove the temporary directory if interrupted
+    (set-signal-handler! signal/int
+                         (lambda (signal)
+                           (delete-path tmp-dir)
+                           (delete-path log-dir)
+                           (exit)))
 
-  (and-let* ((verbosity (cmd-line-arg '--verbosity args)))
-    (set! *verbosity*
-          (or (string->number verbosity) default-verbosity)))
+    (and-let* ((verbosity (cmd-line-arg '--verbosity args)))
+      (set! *verbosity*
+            (or (string->number verbosity) default-verbosity)))
 
-  ;; Remove old log
-  (delete-file* log-file)
+    ;; Remove old log
+    (delete-file* log-file)
 
-  ;; Run salmonellas
-  (let ((egg-slices (split-eggs eggs instances)))
+    ;; Run salmonellas
+    (let ((egg-slices (split-eggs eggs instances)))
+      (let loop ((i instances))
+        (unless (zero? i)
+          (when (> *verbosity* 0) (print "Running instance " i "."))
+          (run-salmonella i
+                          (list-ref egg-slices (- i 1))
+                          chicken-installation-prefix
+                          salmonella-prefix
+                          chicken-install-args
+                          skip-eggs
+                          eggs-source-dir
+                          eggs-doc-dir
+                          keep-repo?
+                          repo-dir
+                          log-dir)
+          (loop (- i 1)))))
+
+    ;; Wait for all salmonellas
     (let loop ((i instances))
       (unless (zero? i)
-        (when (> *verbosity* 0) (print "Running instance " i "."))
-        (run-salmonella i
-                        (list-ref egg-slices (- i 1))
-                        chicken-installation-prefix
-                        salmonella-prefix
-                        chicken-install-args
-                        skip-eggs
-                        eggs-source-dir
-                        eggs-doc-dir
-                        keep-repo?
-                        repo-dir
-                        log-dir)
-        (loop (- i 1)))))
+        (process-wait)
+        (loop (- i 1))))
 
-  ;; Wait for all salmonellas
-  (let loop ((i instances))
-    (unless (zero? i)
-      (process-wait)
-      (loop (- i 1))))
-
-  ;; Merge logs
-  (merge-logs salmonella-prefix log-dir log-file instances)
-  (delete-path log-dir))
+    ;; Merge logs
+    (merge-logs salmonella-prefix log-dir log-file instances)
+    (delete-path log-dir)))
