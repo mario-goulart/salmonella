mario-goulart/salmonella

commit f644914dc6aa31c2c0d244ac77873bfb1838deed

Import chicken.platform for chicken-home

salmonella.scm
@@ -598,7 +598,9 @@
 
       (when clear-chicken-home?
         (let ((chicken-home
-               (shell-command-output csi '(-np "\"(chicken-home)\""))))
+               (shell-command-output
+                csi
+                '(-np "\"(begin (import (chicken home)) (chicken-home))\""))))
           (for-each delete-path
                     (glob (make-pathname chicken-home "*.scm"))))))
 

commit b05e3e9fdee1a0660a12940d63161b353cb38756

Fix stupid typo (s/home/platform/)
Typo introduced in f644914.

salmonella.scm
@@ -600,7 +600,7 @@
         (let ((chicken-home
                (shell-command-output
                 csi
-                '(-np "\"(begin (import (chicken home)) (chicken-home))\""))))
+                '(-np "\"(begin (import (chicken platform)) (chicken-home))\""))))
           (for-each delete-path
                     (glob (make-pathname chicken-home "*.scm"))))))
 