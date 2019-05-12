mario-goulart/salmonella

commit 13ff1e24783eab2fadc60c07874a61b65cd11b20

Only clear chicken-home after init-repo
To get chicken-home, we need to import chicken.platform, which
requires some environment variable settings (done in init-repo).

salmonella-cmd.scm
@@ -260,6 +260,8 @@ EOF
 
        (salmonella 'init-repo!)
 
+       (unless clear-chicken-home? (salmonella 'clear-chicken-home!))
+
        ;; Fetch egg
        (progress-indicator 'fetch egg verbosity egg-count total-eggs)
        (let ((fetch-log (salmonella 'fetch egg)))

salmonella.scm
@@ -594,15 +594,15 @@
       (when (file-exists? tmp-repo-dir)
         (for-each delete-path
                   (glob (make-pathname tmp-repo-dir "*"))))
-      (delete-path (make-pathname tmp-dir egg))
+      (delete-path (make-pathname tmp-dir egg)))
 
-      (when clear-chicken-home?
-        (let ((chicken-home
-               (shell-command-output
-                csi
-                '(-np "\"(begin (import (chicken platform)) (chicken-home))\""))))
-          (for-each delete-path
-                    (glob (make-pathname chicken-home "*.scm"))))))
+    (define (clear-chicken-home!)
+      (let ((chicken-home
+             (shell-command-output
+              csi
+              '(-np "\"(begin (import (chicken platform)) (chicken-home))\""))))
+        (for-each delete-path
+                  (glob (make-pathname chicken-home "*.scm")))))
 
     (define (env-info)
       (define (show-envvar var)
@@ -669,6 +669,8 @@ EOF
 
       (case action
         ((clear-repo!) (clear-repo! egg))
+
+        ((clear-chicken-home!) (clear-chicken-home!))
 
         ((init-repo!) (init-repo!))
 

commit 68652726e455b10ec965f48c4bb0d20fdc988d58

salmonella-cmd.scm: only call clear-chicken-home! in C5
Only call clear-chicken-home! in C5 and when --clear-chicken-home is
provided on the command line.  Also fixes a bug introduced by
13ff1e24783eab2fadc60c07874a61b65cd11b20 (inverted condition).

salmonella-cmd.scm
@@ -6,7 +6,8 @@
   (import chicken)
   (use setup-api) ;; for installation-prefix
   (use data-structures srfi-13)
-  (use salmonella salmonella-log-parser))
+  (use salmonella salmonella-log-parser)
+  (define chicken-4? #t))
  (chicken-5
   (import (chicken base)
           (chicken file)
@@ -19,7 +20,8 @@
           (chicken time))
   (import salmonella salmonella-log-parser)
   (include "libs/srfi-1.scm")
-  (include "libs/srfi-13.scm"))
+  (include "libs/srfi-13.scm")
+  (define chicken-4? #f))
  (else
   (error "Unsupported CHICKEN version.")))
 
@@ -274,7 +276,8 @@ EOF
 
        (salmonella 'init-repo!)
 
-       (unless clear-chicken-home? (salmonella 'clear-chicken-home!))
+       (when (and (not chicken-4?) clear-chicken-home?)
+         (salmonella 'clear-chicken-home!))
 
        ;; Fetch egg
        (progress-indicator 'fetch egg verbosity egg-count total-eggs)