mario-goulart/salmonella

commit 5e4909042f3a4ba1fc9ec2645435e71c8da5b76d

Refactor command line parser
Refactor the command line parser, so that it now can detect invalid
command line options.  The syntax of the command line has been
preserved.

Piggyback:

* properly handle .egg/.setup files in salmonella-cmd
* handle help options in salmonella-log-merger

salmonella-cmd.scm
@@ -158,32 +158,42 @@ EOF
                )))))
 
 
-(let* ((args (command-line-arguments)))
-  (when (or (member "-h" args)
-            (member "--help" args))
+(let* ((parsed-args (parse-cmd-line (command-line-arguments)
+                                    '(-h
+                                      --help
+                                      --version
+                                      (--chicken-installation-prefix)
+                                      (--log-file)
+                                      (--chicken-install-args)
+                                      (--eggs-doc-dir)
+                                      (--skip-eggs)
+                                      (--instance-id)
+                                      --keep-repo
+                                      --clear-chicken-home
+                                      (--repo-dir)
+                                      (--verbosity))))
+       (eggs (map string->symbol (car parsed-args)))
+       (args (cdr parsed-args)))
+
+  (when (or (cmd-line-arg '-h args)
+            (cmd-line-arg '--help args))
     (usage exit-code: 0))
 
-  (when (member "--version" args)
+  (when (cmd-line-arg '--version args)
     (print salmonella-version)
     (exit 0))
 
-  (let* ((this-egg? (and (null? (remove (lambda (arg)
-                                          (string-prefix? "--" arg))
-                                        args))
-                         (not (null? (glob "*.egg")))))
+  (let* ((this-egg? (null? eggs))
          (chicken-installation-prefix
           (cmd-line-arg '--chicken-installation-prefix args))
          (log-file (or (cmd-line-arg '--log-file args) "salmonella.log"))
          (chicken-install-args
           (cmd-line-arg '--chicken-install-args args))
-         (eggs-doc-dir
-          (cmd-line-arg '--eggs-doc-dir args))
-         (skip-eggs (let ((skip (cmd-line-arg '--skip-eggs args)))
-                      (if skip
-                          (map string->symbol (string-split skip ","))
-                          '())))
-         (keep-repo? (and (member "--keep-repo" args) #t))
-         (clear-chicken-home? (and (member "--clear-chicken-home" args) #t))
+         (skip-eggs (or (and-let* ((skip (cmd-line-arg '--skip-eggs args)))
+                          (map string->symbol (string-split skip ",")))
+                        '()))
+         (keep-repo? (cmd-line-arg '--keep-repo args))
+         (clear-chicken-home? (cmd-line-arg '--clear-chicken-home args))
          (repo-dir (and-let* ((path (cmd-line-arg '--repo-dir args)))
                      (if (absolute-pathname? path)
                          path
@@ -192,171 +202,170 @@ EOF
          (tmp-dir (or repo-dir (mktempdir)))
          (verbosity (or (and-let* ((verbosity (cmd-line-arg '--verbosity args)))
                           (or (string->number verbosity) default-verbosity))
-                        default-verbosity))
-         (salmonella (make-salmonella
-                      tmp-dir
-                      eggs-doc-dir: eggs-doc-dir
-                      chicken-installation-prefix: chicken-installation-prefix
-                      clear-chicken-home?: clear-chicken-home?
-                      chicken-install-args:
-                      (and chicken-install-args
-                           (lambda (repo)
-                             (list
-                              (or (irregex-replace "<repo>" chicken-install-args repo)
-                                  chicken-install-args))))
-                      this-egg?: this-egg?))
-         (eggs (if this-egg?
-                   (let ((egg-spec (glob "*.egg")))
-                     (cond ((null? egg-spec)
-                            (die "Could not find a .egg file. Aborting."))
-                           ((null? (cdr egg-spec))
-                            (map (compose string->symbol pathname-file) egg-spec))
-                           (else
-                            (die "Found more than one .egg file.  Aborting."))))
-                   (remove (lambda (egg)
-                             (memq egg skip-eggs))
-                           (map string->symbol
-                                (remove (lambda (arg)
-                                          (string-prefix? "--" arg))
-                                        args)))))
-         (total-eggs (length eggs)))
+                        default-verbosity)))
 
-    (when (null? eggs)
+    (when this-egg?
+      (let* ((suffix (cond-expand
+                      (chicken-4 ".setup")
+                      (chicken-5 ".egg")))
+             (egg-spec (glob (string-append "*" suffix))))
+        (cond ((null? egg-spec)
+               (die (sprintf "Could not find a ~a file. Aborting." suffix)))
+              ((null? (cdr egg-spec))
+               (set! eggs (map (compose string->symbol pathname-file) egg-spec)))
+              (else
+               (die (sprintf "Found more than one ~a file.  Aborting." suffix))))))
+
+    (when (and (null? eggs) (not this-egg?))
       (delete-path tmp-dir)
       (print "Nothing to do.")
       (exit))
 
+    (let ((salmonella
+           (make-salmonella
+            tmp-dir
+            eggs-doc-dir: (cmd-line-arg '--eggs-doc-dir args)
+            chicken-installation-prefix: chicken-installation-prefix
+            clear-chicken-home?: clear-chicken-home?
+            chicken-install-args:
+            (and chicken-install-args
+                 (lambda (repo)
+                   (list
+                    (or (irregex-replace "<repo>" chicken-install-args repo)
+                        chicken-install-args))))
+            this-egg?: this-egg?))
+          (total-eggs (length eggs)))
 
-    ;; Remove the temporary directory if interrupted
-    (set-signal-handler! signal/int
-                         (lambda (signal)
-                           (delete-path tmp-dir)
-                           (exit)))
+      ;; Remove the temporary directory if interrupted
+      (set-signal-handler! signal/int
+                           (lambda (signal)
+                             (delete-path tmp-dir)
+                             (exit)))
 
-    ;; for salmonella-epidemy
-    (set! *instance-id* (cmd-line-arg '--instance-id args))
+      ;; for salmonella-epidemy
+      (set! *instance-id* (cmd-line-arg '--instance-id args))
 
-    ;; Remove old log
-    (delete-file* log-file)
+      ;; Remove old log
+      (delete-file* log-file)
 
-    ;; Initialize salmonella's repo here.  It must be done before we
-    ;; try to use the environment variables, as init-repo! sets them.
-    (salmonella 'init-repo!)
+      ;; Initialize salmonella's repo here.  It must be done before we
+      ;; try to use the environment variables, as init-repo! sets them.
+      (salmonella 'init-repo!)
 
-    (when (> verbosity 1)
-      (print (salmonella 'env-info)))
+      (when (> verbosity 1)
+        (print (salmonella 'env-info)))
 
-    ;; Log start
-    (log! (make-report #f 'start 0 (salmonella 'env-info) (current-seconds))
-          log-file)
+      ;; Log start
+      (log! (make-report #f 'start 0 (salmonella 'env-info) (current-seconds))
+            log-file)
 
-    ;; Log version
-    (log! (make-report #f 'log-version 0 salmonella-log-version 0)
-          log-file)
+      ;; Log version
+      (log! (make-report #f 'log-version 0 salmonella-log-version 0)
+            log-file)
 
-    ;; Log skipped eggs
-    (for-each (lambda (egg)
-                (log! (make-report egg 'skip 0 "" 0) log-file))
-              skip-eggs)
+      ;; Log skipped eggs
+      (for-each (lambda (egg)
+                  (log! (make-report egg 'skip 0 "" 0) log-file))
+                skip-eggs)
 
-    ;; Maybe show warning about existing Scheme files in chicken-home
-    (let ((msg (check-chicken-home chicken-installation-prefix this-egg?)))
-      (when msg
-        (with-output-to-port (current-error-port)
-          (cut print msg))))
+      ;; Maybe show warning about existing Scheme files in chicken-home
+      (let ((msg (check-chicken-home chicken-installation-prefix this-egg?)))
+        (when msg
+          (with-output-to-port (current-error-port)
+            (cut print msg))))
 
-    ;; Handle all eggs
-    (for-each
-     (lambda (egg egg-count)
+      ;; Handle all eggs
+      (for-each
+       (lambda (egg egg-count)
 
-       (unless keep-repo? (salmonella 'clear-repo!))
+         (unless keep-repo? (salmonella 'clear-repo!))
 
-       (salmonella 'init-repo!)
+         (salmonella 'init-repo!)
 
-       (when (and (not chicken-4?) clear-chicken-home?)
-         (salmonella 'clear-chicken-home!))
+         (when (and (not chicken-4?) clear-chicken-home?)
+           (salmonella 'clear-chicken-home!))
 
-       ;; Fetch egg
-       (progress-indicator 'fetch egg verbosity egg-count total-eggs)
-       (let ((fetch-log (salmonella 'fetch egg)))
-         (log! fetch-log log-file)
-         (status-reporter fetch-log verbosity)
+         ;; Fetch egg
+         (progress-indicator 'fetch egg verbosity egg-count total-eggs)
+         (let ((fetch-log (salmonella 'fetch egg)))
+           (log! fetch-log log-file)
+           (status-reporter fetch-log verbosity)
 
-         (when (zero? (report-status fetch-log))
+           (when (zero? (report-status fetch-log))
 
-           ;; Meta data
-           (progress-indicator 'meta-data egg verbosity)
-           (let ((meta-log (salmonella 'meta-data egg)))
-             (log! meta-log log-file)
-             (status-reporter meta-log verbosity)
+             ;; Meta data
+             (progress-indicator 'meta-data egg verbosity)
+             (let ((meta-log (salmonella 'meta-data egg)))
+               (log! meta-log log-file)
+               (status-reporter meta-log verbosity)
 
-             (when (report-status meta-log)
-               (let ((meta-data (report-message meta-log)))
+               (when (report-status meta-log)
+                 (let ((meta-data (report-message meta-log)))
 
-                 ;; Warnings (only logged when indicate problems)
+                   ;; Warnings (only logged when indicate problems)
 
-                 ;; Check dependencies
-                 (progress-indicator 'check-dependencies egg verbosity)
-                 (let ((deps-log (salmonella 'check-dependencies egg meta-data)))
-                   (unless (report-status deps-log)
-                     (log! deps-log log-file))
-                   (status-reporter deps-log verbosity))
+                   ;; Check dependencies
+                   (progress-indicator 'check-dependencies egg verbosity)
+                   (let ((deps-log (salmonella 'check-dependencies egg meta-data)))
+                     (unless (report-status deps-log)
+                       (log! deps-log log-file))
+                     (status-reporter deps-log verbosity))
 
-                 ;; Check category
-                 (progress-indicator 'check-category egg verbosity)
-                 (let ((categ-log (salmonella 'check-category egg meta-data)))
-                   (unless (report-status categ-log)
-                     (log! categ-log log-file))
-                   (status-reporter categ-log verbosity))
+                   ;; Check category
+                   (progress-indicator 'check-category egg verbosity)
+                   (let ((categ-log (salmonella 'check-category egg meta-data)))
+                     (unless (report-status categ-log)
+                       (log! categ-log log-file))
+                     (status-reporter categ-log verbosity))
 
-                 ;; Check license
-                 (progress-indicator 'check-license egg verbosity)
-                 (let ((license-log (salmonella 'check-license egg meta-data)))
-                   (unless (report-status license-log)
-                     (log! license-log log-file))
-                   (status-reporter license-log verbosity))
+                   ;; Check license
+                   (progress-indicator 'check-license egg verbosity)
+                   (let ((license-log (salmonella 'check-license egg meta-data)))
+                     (unless (report-status license-log)
+                       (log! license-log log-file))
+                     (status-reporter license-log verbosity))
 
-                 ;; Check author
-                 (progress-indicator 'check-author egg verbosity)
-                 (let ((author-log (salmonella 'check-author egg meta-data)))
-                   (unless (report-status author-log)
-                     (log! author-log log-file))
-                   (status-reporter author-log verbosity))
+                   ;; Check author
+                   (progress-indicator 'check-author egg verbosity)
+                   (let ((author-log (salmonella 'check-author egg meta-data)))
+                     (unless (report-status author-log)
+                       (log! author-log log-file))
+                     (status-reporter author-log verbosity))
 
-                 ;; Install egg
-                 (progress-indicator 'install egg verbosity)
-                 (let ((install-log (salmonella 'install egg)))
-                   (log! install-log log-file)
-                   (status-reporter install-log verbosity)
+                   ;; Install egg
+                   (progress-indicator 'install egg verbosity)
+                   (let ((install-log (salmonella 'install egg)))
+                     (log! install-log log-file)
+                     (status-reporter install-log verbosity)
 
-                   (when (zero? (report-status install-log))
-                     ;; Check version
-                     (let ((check-version-log (salmonella 'check-version egg)))
-                       (progress-indicator 'check-version egg verbosity)
-                       (log! check-version-log log-file)
-                       (status-reporter check-version-log verbosity))
+                     (when (zero? (report-status install-log))
+                       ;; Check version
+                       (let ((check-version-log (salmonella 'check-version egg)))
+                         (progress-indicator 'check-version egg verbosity)
+                         (log! check-version-log log-file)
+                         (status-reporter check-version-log verbosity))
 
-                     ;; Test egg
-                     (progress-indicator 'test egg verbosity)
-                     (let ((test-logs (salmonella 'test egg)))
-                       (for-each (lambda (test-log)
-                                   (log! test-log log-file)
-                                   (when (eq? (report-action test-log) 'test)
-                                     (status-reporter test-log verbosity)))
-                                 test-logs)))))))))
+                       ;; Test egg
+                       (progress-indicator 'test egg verbosity)
+                       (let ((test-logs (salmonella 'test egg)))
+                         (for-each (lambda (test-log)
+                                     (log! test-log log-file)
+                                     (when (eq? (report-action test-log) 'test)
+                                       (status-reporter test-log verbosity)))
+                                   test-logs)))))))))
 
-       ;; Check doc
-       (progress-indicator 'check-doc egg verbosity)
-       (let ((doc-log (salmonella 'check-doc egg)))
-         (log! doc-log log-file)
-         (status-reporter doc-log verbosity)))
+         ;; Check doc
+         (progress-indicator 'check-doc egg verbosity)
+         (let ((doc-log (salmonella 'check-doc egg)))
+           (log! doc-log log-file)
+           (status-reporter doc-log verbosity)))
 
-     eggs
-     (iota total-eggs 1))
+       eggs
+       (iota total-eggs 1))
 
-    (log! (make-report #f 'end 0 "" (current-seconds)) log-file)
-    (unless this-egg?
-      (show-statistics log-file verbosity))
-    (unless keep-repo? (delete-path tmp-dir))))
+      (log! (make-report #f 'end 0 "" (current-seconds)) log-file)
+      (unless this-egg?
+        (show-statistics log-file verbosity))
+      (unless keep-repo? (delete-path tmp-dir)))))
 
 )

salmonella-common.scm
@@ -1,8 +1,8 @@
 (import scheme)
 (cond-expand
  (chicken-4
-  (import foreign irregex)
-  (use extras  files ports posix srfi-1)
+  (import foreign)
+  (use extras files irregex ports posix srfi-1 srfi-13)
   (define pseudo-random-integer random))
  (chicken-5
   (import (chicken file)
@@ -13,7 +13,9 @@
           (chicken port)
           (chicken process-context)
           (chicken random)
-          (chicken string)))
+          (chicken string))
+  (include "libs/srfi-1.scm")
+  (include "libs/srfi-13.scm"))
  (else
   (error "Unsupported CHICKEN version.")))
 
@@ -84,18 +86,57 @@
                    (deps 'test-dependencies)
                    '()))))
 
+(define (parse-cmd-line cmd-line-args spec)
+  ;; spec: list of elements.  The format of elements is:
+  ;;  - symbols: options that do not require an argument
+  ;;  - lists: options that require an argument.
+  ;;  - what doesn't match these patterns is assumed to be a no named arg
+  ;; Return a pair (<eggs> . <alist opts>)
+  ;; Note: options are supposed to start with `--'.  -h is specially handled.
+  (let ((nonamed '())
+        (parsed-opts '()))
+    (let loop ((args cmd-line-args))
+      (unless (null? args)
+        (let ((arg (car args)))
+          (cond ((string-prefix? "--" arg)
+                 (if (substring-index "=" arg)
+                     (let* ((parts (string-split arg "="))
+                            (param (string->symbol (car parts)))
+                            (val (string-intersperse (cdr parts) "=")))
+                       (if (memq param spec)
+                           (die param " does not take any argument.")
+                           (if (alist-ref param (filter pair? spec))
+                               (set! parsed-opts (cons (cons param val) parsed-opts))
+                               (die "Invalid option: " param))))
+                     (let ((param (string->symbol arg)))
+                       (if (memq param spec)
+                           (set! parsed-opts (cons (cons param #t)  parsed-opts))
+                           (if (alist-ref param (filter pair? spec))
+                               (die param " requires an argument.")
+                               (die "Invalid option: " param))))))
+                ((string=? arg "-h")
+                 (set! parsed-opts (cons (cons '-h #t) parsed-opts)))
+                (else
+                 (set! nonamed (cons arg nonamed)))))
+        (loop (cdr args))))
+    (cons nonamed parsed-opts)))
 
-(define (cmd-line-arg option args)
+(define (cmd-line-arg option parsed-opts)
   ;; Returns the argument associated to the command line option OPTION
-  ;; in ARGS or #f if OPTION is not found in ARGS or doesn't have any
-  ;; argument.
-  (let ((val (any (lambda (arg)
-                    (irregex-match
-                     `(seq ,(->string option) "=" (submatch (* any)))
-                     arg))
-                  args)))
-    (and val (irregex-match-substring val 1))))
-
+  ;; in ARGS or #f if OPTION is not found in ARGS.  ARGS is the cdr of
+  ;; the pair returned by parse-cmd-line.
+  (let loop ((opts parsed-opts))
+    (if (null? opts)
+        #f
+        (let* ((opt (car opts))
+               (param (if (pair? opt)
+                          (car opt)
+                          opt)))
+          (cond ((and (pair? opt) (eq? (car opt) option))
+                 (cdr opt))
+                ((and (symbol? opt) (eq? opt option))
+                 #t)
+                (else (loop (cdr opts))))))))
 
 (define (die . msg)
   (with-output-to-port (current-error-port)
@@ -199,7 +240,6 @@ EOF
 
 --instances=<number>
     Number of salmonella instances to run in parallel.
-
 EOF
 ))
     (newline)

salmonella-epidemy.scm
@@ -95,13 +95,31 @@
     (system cmd)))
 
 
-(let ((args (command-line-arguments)))
-  (when (or (member "-h" args)
-            (member "--help" args))
+(let* ((parsed-args (parse-cmd-line (command-line-arguments)
+                                    '(-h
+                                      --help
+                                      --version
+                                      (--chicken-installation-prefix)
+                                      (--log-file)
+                                      (--chicken-install-args)
+                                      (--eggs-doc-dir)
+                                      (--skip-eggs)
+                                      (--instance-id)
+                                      --keep-repo
+                                      --clear-chicken-home
+                                      (--repo-dir)
+                                      (--verbosity))))
+       (eggs (map string->symbol (car parsed-args)))
+       (args (cdr parsed-args)))
+
+  (when (or (cmd-line-arg '-h args)
+            (cmd-line-arg '--help args))
     (usage exit-code: 0 epidemy?: #t))
-  (when (member "--version" args)
+
+  (when (cmd-line-arg '--version args)
     (print salmonella-version)
     (exit 0))
+
   (let* ((chicken-installation-prefix
           (cmd-line-arg '--chicken-installation-prefix args))
          (chicken-install-args
@@ -113,8 +131,8 @@
                       (if skip
                           (map string->symbol (string-split skip ","))
                           '())))
-         (keep-repo? (and (member "--keep-repo" args) #t))
-         (clear-chicken-home? (and (member "--clear-chicken-home" args) #t))
+         (keep-repo? (cmd-line-arg '--keep-repo args))
+         (clear-chicken-home? (cmd-line-arg '--clear-chicken-home args))
          (repo-dir (or (and-let* ((path (cmd-line-arg '--repo-dir args)))
                          (if (absolute-pathname? path)
                              path
@@ -127,12 +145,6 @@
                             (normalize-pathname
                              (make-pathname (current-directory) path))))
                       (mktempdir)))
-         (eggs (remove (lambda (egg)
-                         (memq egg skip-eggs))
-                       (map string->symbol
-                            (remove (lambda (arg)
-                                      (string-prefix? "--" arg))
-                                    args))))
          (total-eggs (length eggs))
          (instances (or (and-let* ((i (cmd-line-arg '--instances args)))
                           (or (string->number i) 1))

salmonella-log-inquirer.scm
@@ -58,10 +58,6 @@ Undocumented: #(count-undocumented log-data)
 EOF
 )
 
-(define (read-log args)
-  (let ((log-file (last args)))
-    (read-log-file log-file)))
-
 (define (usage #!optional exit-code)
     (let ((this (pathname-strip-directory (program-name)))
           (port (if (and exit-code (not (zero? exit-code)))
@@ -86,44 +82,57 @@ EOF
     (when exit-code (exit exit-code))))
 
 
-(let ((args (command-line-arguments)))
-  (cond
-   ((null? args)
-    (usage 1))
-   ((or (member "-h" args)
-        (member "-help" args)
-        (member "--help" args))
+(let* ((parsed-args (parse-cmd-line (command-line-arguments)
+                                    '(-h
+                                      --help
+                                      --version
+                                      --list-eggs
+                                      --log-info
+                                      --statistics
+                                      (--action)
+                                      (--egg)
+                                      (--part))))
+       (log-files (car parsed-args))
+       (args (cdr parsed-args)))
+
+  (when (or (cmd-line-arg '-h args)
+            (cmd-line-arg '--help args))
     (usage 0))
-   ((member "--version" args)
-    (print salmonella-version))
-   ((null? (cdr args)) ;; from now on we require the log file
+
+  (when (cmd-line-arg '--version args)
+    (print salmonella-version)
+    (exit 0))
+
+  (when (null? log-files)
     (usage 1))
-   ((member "--list-eggs" args)
-    (for-each print (log-eggs (read-log args))))
-   ((member "--log-info" args)
-    (print (salmonella-info (read-log args))))
-   ((member "--statistics" args)
-    (print (log-statistics (read-log args))))
-   (else
-    (let ((action-str (cmd-line-arg '--action args))
-          (egg-str  (cmd-line-arg '--egg args))
-          (part-str (cmd-line-arg '--part args)))
-      (unless action-str
-        (die "Missing --action=<action>"))
-      (unless egg-str
-        (die "Missing --egg=<egg>"))
-      (let ((action (string->symbol action-str))
-            (egg (string->symbol egg-str))
-            (part (if part-str
-                      (string->symbol part-str)
-                      'message)))
-        (unless (memq action valid-actions)
-          (die "Invalid action: " action))
-        (unless (memq part valid-parts)
-          (die "Invalid part: " part))
-        (let ((printer (if (eq? action 'meta-data)
-                           pp
-                           print)))
-          (printer (query-action (read-log args) egg action part)))))))
-  )
+
+  (let ((log-file (car log-files)))
+    (cond
+     ((cmd-line-arg '--list-eggs args)
+      (for-each print (log-eggs (read-log-file log-file))))
+     ((cmd-line-arg '--log-info args)
+      (print (salmonella-info (read-log-file log-file))))
+     ((cmd-line-arg '--statistics args)
+      (print (log-statistics (read-log-file log-file))))
+     (else
+      (let ((action-str (cmd-line-arg '--action args))
+            (egg-str  (cmd-line-arg '--egg args))
+            (part-str (cmd-line-arg '--part args)))
+        (unless action-str
+          (die "Missing --action=<action>"))
+        (unless egg-str
+          (die "Missing --egg=<egg>"))
+        (let ((action (string->symbol action-str))
+              (egg (string->symbol egg-str))
+              (part (if part-str
+                        (string->symbol part-str)
+                        'message)))
+          (unless (memq action valid-actions)
+            (die "Invalid action: " action))
+          (unless (memq part valid-parts)
+            (die "Invalid part: " part))
+          (let ((printer (if (eq? action 'meta-data)
+                             pp
+                             print)))
+            (printer (query-action (read-log-file log-file) egg action part)))))))))
 ) ;; end module

salmonella-log-merger.scm
@@ -66,17 +66,26 @@
 
 
 
-(let ((args (command-line-arguments)))
-  (when (null? args)
+(let* ((parsed-args (parse-cmd-line (command-line-arguments)
+                                    '(-h
+                                      --help
+                                      --version
+                                      (--log-file))))
+       (log-files (car parsed-args))
+       (args (cdr parsed-args)))
+
+  (when (or (cmd-line-arg '-h args)
+            (cmd-line-arg '--help args))
+    (usage 0))
+
+  (when (and (null? args) (null? log-files))
     (usage 1))
-  (when (member "--version" args)
+
+  (when (cmd-line-arg '--version args)
     (print salmonella-version)
     (exit 0))
-  (let ((log-files
-         (remove (lambda (arg)
-                   (string-prefix? "--" arg))
-                 args))
-        (out-file (cmd-line-arg '--log-file args)))
+
+  (let ((out-file (cmd-line-arg '--log-file args)))
     (when (file-exists? out-file)
       (die out-file " already exists. Aborting."))
     (unless out-file

commit cc58129c744224720920cba47aa1592018e56ecd

Fix --skip-eggs
Broken by 5e4909042f3a4ba1fc9ec2645435e71c8da5b76d

salmonella-cmd.scm
@@ -173,7 +173,7 @@ EOF
                                       --clear-chicken-home
                                       (--repo-dir)
                                       (--verbosity))))
-       (eggs (map string->symbol (car parsed-args)))
+       (all-eggs (map string->symbol (car parsed-args)))
        (args (cdr parsed-args)))
 
   (handle-help args
@@ -182,7 +182,7 @@ EOF
 
   (handle-version args)
 
-  (let* ((this-egg? (null? eggs))
+  (let* ((this-egg? (null? all-eggs))
          (chicken-installation-prefix
           (cmd-line-arg '--chicken-installation-prefix args))
          (log-file (or (cmd-line-arg '--log-file args) "salmonella.log"))
@@ -191,6 +191,9 @@ EOF
          (skip-eggs (or (and-let* ((skip (cmd-line-arg '--skip-eggs args)))
                           (map string->symbol (string-split skip ",")))
                         '()))
+         (eggs (remove (lambda (egg)
+                         (memq egg skip-eggs))
+                       all-eggs))
          (keep-repo? (cmd-line-arg '--keep-repo args))
          (clear-chicken-home? (cmd-line-arg '--clear-chicken-home args))
          (repo-dir (and-let* ((path (cmd-line-arg '--repo-dir args)))